9c99c9bb02784bc0a9aaefca7867ec46ca0f43e5
Brett Loewen <brettzky007@gmail.com>
2025-08-19 14:24:54 -0500

    Initial commit

---
+++ src/main/java/net/supremetor/prismarine/Prismarine.java
@@ -0,0 +1,15 @@
+package net.supremetor.prismarine;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Prismarine implements ModInitializer {
+	public static final String MOD_ID = "prismarine";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/supremetor/prismarine/PrismarineClient.java
@@ -0,0 +1,10 @@
+package net.supremetor.prismarine;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PrismarineClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
